7d695634f626536247f33b03a55269df2a8cf300
unknown <adriagp90@gmail.com>
2024-03-08 16:30:16 +0100

    segon commit classe

--- TIC_BCN_AGP/centre/views.py
+++ TIC_BCN_AGP/centre/views.py
@@ -5,7 +5,48 @@ def index(request):
     return render(request, 'index.html')
 
 def students(request):
-    return render(request, 'students.html')
+    students = [
+        {
+            'nom':'Adria',
+            'cognom1':'Garcia',
+            'cognom2':'Perez',
+            'email':'adria.garcia@iticbcn.cat',
+            'curs':'DAW2A',
+            'moduls': ['M06', 'M07', 'M08', 'M09']
+        }
+    ]
+    context = { 'students': students }
+    return render(request, 'students.html', context)
 
 def teachers(request):
-    return render(request, 'teachers.html')
+    teachers = [
+        {
+            'nom': 'Juanma',
+            'cognom1': 'Sanchez',
+            'cognom2': 'Bel',
+            'email': 'juanmanuel.sanchez@iticbcn.cat',
+            'curs': ['DAM2B', 'DAW2A'],
+            'tutor': True,
+            'modul': 'M06'
+        },
+        {
+            'nom': 'Roger',
+            'cognom1': 'Sobrino',
+            'cognom2': 'Gil',
+            'email': 'roger.sobrino@iticbcn.cat',
+            'curs': ['DAM2B', 'DAW2A'],
+            'tutor': False,
+            'modul': 'M07'
+        },
+        {
+            'nom': 'Josep Oriol',
+            'cognom1': 'Roca',
+            'cognom2': 'Fabra',
+            'email': 'joseporiol.rpca@iticbcn.cat',
+            'curs': ['DAM2B', 'DAW2A'],
+            'tutor': False,
+            'modul': 'M09'
+        }
+    ]
+    context = teachers
+    return render(request, 'teachers.html', context)

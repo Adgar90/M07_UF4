1eeee3ef26517f22668cd0efe2505e6cd719ccfb
Adrià García Pérez <adriagp90@gmail.com>
2024-03-17 00:22:58 +0100

    creació model User

--- TIC_BCN_AGP/centre/models.py
+++ TIC_BCN_AGP/centre/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    ROLES = [
+        ("S", "Student"),
+        ("T", "Teacher")
+    ]
+    nom = models.CharField(max_length=30)
+    cognom1 = models.CharField(max_length=30)
+    cognom2 = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    curs = models.CharField(max_length=30)
+    moduls = models.CharField(max_length=30)
+    role = models.CharField(max_length=30, choices=ROLES)
